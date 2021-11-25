8781c2e2065d1d2bc03db3bce5052fd9a170886a
Daria Starichkova <dstarichkova@gmail.com>
2021-11-25 22:51:06 +0300

    Change interface into class & add random colors

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import sys
 from random import randint
-from PyQt5 import uic
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
 
 
-class Example(QWidget):
+class Ui_widget(object):
+    def setupUi(self, widget):
+        widget.setObjectName("widget")
+        widget.resize(400, 400)
+        self.btn = QtWidgets.QPushButton(widget)
+        self.btn.setGeometry(QtCore.QRect(10, 10, 151, 32))
+        self.btn.setObjectName("btn")
+
+        self.retranslateUi(widget)
+        QtCore.QMetaObject.connectSlotsByName(widget)
+
+    def retranslateUi(self, widget):
+        _translate = QtCore.QCoreApplication.translate
+        widget.setWindowTitle(_translate("widget", "Нарисовать круги"))
+        self.btn.setText(_translate("widget", "Нарисовать круги"))
+
+
+class Example(QWidget, Ui_widget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.btn.clicked.connect(self.paint)
 
@@ -25,7 +42,7 @@ class Example(QWidget):
 
     def draw_flag(self, qp):
         for i in range(randint(1, 10)):
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             radius = randint(1, 200)
             qp.drawEllipse(randint(1, 400), randint(1, 400), radius, radius)
 
